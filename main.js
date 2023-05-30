c97dda5f0644f9b1b7fcf504364d99698d8b8d89
jamesdurai <jamesdurai@outlook.com>
2023-05-30 10:49:22 +0530

    loop fibonacci series

---
+++ main.js
@@ -0,0 +1,12 @@
+let f=parseInt(prompt("Enter number"));
+a=0;
+b=1;
+document.write("Here is the Fibonacci series upto to "+f +" is");
+document.write("<br>");
+document.write(a+" "+b);
+for(i=1;i<=f;i++){
+	sum=a+b;
+	a=b;
+	b=sum;
+	document.write(" "+sum+" ");
+}
